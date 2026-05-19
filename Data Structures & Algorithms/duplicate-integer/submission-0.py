04122ec1a2559a8624bfa67d229deae3db7036b9
Krishnakumar Mishra <krishnamishra9404@gmail.com>
2026-05-19 21:37:50 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        """
+        > This function accepts a list of integers.
+        > It checks if any list item (integer) appears more than once in the list.
+            - If more than once, return True
+            - Else return False
+        > E.g.:
+            - [1,2,3,3] => True
+            - [2,3,4,5] => False
+        """
+
+        if not isinstance(nums, list):
+            return TypeError("Provided value is not a list")
+        
+        '''
+        Logic buidling approaches:
+        1. Using set():
+            > We create deduplicated list of nums (using set)
+            > If length of deduplicated list is less than original list, 
+              then it proves that original list had repeated numbers
+            > Time complexity: O(n) for set operation
+
+        2. Using count():
+            > For each element of list, run count() of that item on list.
+            > If item count is more than 1, it means list has duplicates
+            > Time complexity: This will become O(n2)
+                - Each loop item, so O(n)
+                - count funtion check against each item of loop, so (n)
+        
+        Going ahead with 1st approach
+        '''
+
+        unique_nums = set(nums)
+        if len(unique_nums) < len(nums):
+            return True
+        else:
+            return False
+
+
